dea40c8a7f586e7988b41ba169d59300c15e1d47
apple <apple@apples-MacBook-Pro.local>
2021-08-23 14:28:28 -0400

    update

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,35 @@
 import "./topbar.scss"
+import {Person,Mail} from "@material-ui/icons"
 
 export default function Topbar() {
     return (
-        <div className="topbar">
-          yessss 
+        <div className="topbar active">
+          <div className="wrapper">
+              <div className="left">
+
+              <a href="#intro" className="logo">rocketman</a>
+              <div className="itemContainer">
+                <Person className="icon"/>
+                <span>555 555 555</span>
+              </div>
+              <div className="itemContainer">
+                <Mail className="icon"/>
+                <span>toddgranados@gmail.com</span>
+              </div>
+              </div>
+              
+              <div className="right">
+                  <div className="hamburger">
+                    <span className="line1"></span>
+                    <span className="line2"></span>
+                    <span className="line3"></span>
+
+
+                  </div>
+
+
+              </div>
+          </div>
         </div>
     )
 }
