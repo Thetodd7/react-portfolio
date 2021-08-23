458f04dfdd786c1163dfe5b5b161f53bfa4ffd00
Todd Granados <Toddgranados@gmail.com>
2021-08-22 22:53:31 -0400

    Add files via upload

---
+++ src/components/works/Works.jsx
@@ -0,0 +1,9 @@
+import "./works.scss"
+
+export default function Works() {
+    return (
+        <div className="works">
+          lets goooo react.js developer here i come 
+        </div>
+    )
+}
