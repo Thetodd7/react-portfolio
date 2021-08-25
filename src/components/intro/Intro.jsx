fd550132ab6a8f1e851af00ddb71228ae17c8546
apple <apple@apples-MacBook-Pro.local>
2021-08-25 16:37:19 -0400

    Added arrow animation

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -4,15 +4,15 @@ export default function Intro() {
   return (
     <div className="intro" id="intro">
       <div className="left">
-         <div className="imgContainer">
-          <img src="assets/man.png" alt=""/>
+        <div className="imgContainer">
+          <img src="assets/man.png" alt="" />
         </div>
       </div>
       <div className="right">
         <div className="wrapper">
-          <h2> Hi There, I'm</h2>
-          <h1>Todd Granados</h1>
-          <h3>Freelance<span></span></h3>
+        <h2> Hi There, I'm</h2>
+        <h1>Todd Granados</h1>
+        <h3>Freelance<span></span></h3>
         </div>
         <a href="#portfolio">
           <img src="assets/down.png" alt="" />
@@ -21,3 +21,8 @@ export default function Intro() {
     </div>
   );
 }
+
+
+
+
+
