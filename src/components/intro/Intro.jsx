82757c45ceac284b8fbb7c7e5ff51f789f99ef5c
apple <apple@apples-MacBook-Pro.local>
2021-08-25 17:37:07 -0400

    update

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,6 +1,19 @@
 import "./intro.scss";
+import { init } from "ityped";
+import { useEffect, useRef } from "react";
 
 export default function Intro() {
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed: 60,
+      strings: ["Developer", "Designer", "Content Creator"],
+    });
+  }, []);
+
   return (
     <div className="intro" id="intro">
       <div className="left">
@@ -10,9 +23,11 @@ export default function Intro() {
       </div>
       <div className="right">
         <div className="wrapper">
-        <h2> Hi There, I'm</h2>
-        <h1>Todd Granados</h1>
-        <h3>Freelance<span></span></h3>
+          <h2> Hi There, I'm</h2>
+          <h1>Todd Granados</h1>
+          <h3>
+            Freelance <span ref={textRef}></span>
+          </h3>
         </div>
         <a href="#portfolio">
           <img src="assets/down.png" alt="" />
@@ -21,8 +36,3 @@ export default function Intro() {
     </div>
   );
 }
-
-
-
-
-
